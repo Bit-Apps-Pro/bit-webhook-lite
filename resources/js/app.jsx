62cb5113c6a1662f612f399eb53c44698b7e635f
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2022-12-20 18:34:55 +0600

    chakra UI added

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,16 +5,25 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {ChakraProvider} from '@chakra-ui/react'
+import Master from './Pages/Layouts/Master'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => {
+        const page =resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx'))
+        page.layout=page.layout || Master
+        return page
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
-
-        root.render(<App {...props} />);
+        root.render(
+        <ChakraProvider>
+            <App {...props} />
+        </ChakraProvider>
+        );
     },
 });
 
