88d4d22215f98e04079927f49e9106a4a0ab65b3
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-01-02 10:17:26 +0600

    Log and URL slug migration file & Model created

---
+++ app/Models/Logs.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Logs extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'url_slug_id',
+        'uuid',
+        'webhook_details',
+    ];
+
+}
---
+++ database/migrations/2022_12_25_083451_create_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('url_slug_id')->constrained('url_slug_generates')->onDelete('cascade');
+            $table->mediumText('uuid')->nullable();
+            $table->json('webhook_details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+};
