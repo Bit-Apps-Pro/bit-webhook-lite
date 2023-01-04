233e856233902d901df2aeb5c7146efd2a6482d8
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-01-04 11:44:26 +0600

    remove unused class

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Console\Application;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-class HomeController extends Controller
-{
-    public function index()
-    {
+class HomeController extends Controller {
+    public function index() {
         return Inertia::render('Home', [
-            'canLogin' => route('login'),
+            'canLogin'    => route('login'),
             'canRegister' => route('register'),
-            'phpVersion' => PHP_VERSION,
+            'phpVersion'  => PHP_VERSION,
         ]);
     }
+
 }
