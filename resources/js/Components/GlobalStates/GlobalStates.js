8686cff464dcc0feba23c010ca66c23f24e60c27
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-01-04 11:42:05 +0600

    set Global state

---
+++ resources/js/Components/GlobalStates/GlobalStates.js
@@ -0,0 +1,16 @@
+import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+
+
+
+// export const $logs = atom(localStorage.getItem("bit_webhook_logs") ? JSON.parse(localStorage.getItem("bit_webhook_logs")) : []);
+export const $logId = atom(0);
+
+export const $logs = atomWithStorage('bit_webhook_logs', [])
+export const $randomUrl = atomWithStorage('bit_random_url', '');
+
+export const $currentLog = atom((get) => {
+    const logs = get($logs);
+    const logId = get($logId);
+    return logs[logId];
+})
