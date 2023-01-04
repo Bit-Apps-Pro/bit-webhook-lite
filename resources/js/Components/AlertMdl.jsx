cc52ef15b2f53a6426d4f038288b00ea4cbd5a8d
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-01-04 11:42:35 +0600

    alert Model component added

---
+++ resources/js/Components/AlertMdl.jsx
@@ -0,0 +1,39 @@
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button } from "@chakra-ui/react";
+import { useRef } from "react";
+
+export default function AlertMdl({ isOpen, setIsOpen,ref, title, body='',canceLabel='No',deleteLabel='Yes', action }) {
+  const cancelRef = useRef()
+
+  const onClose = () => setIsOpen(false);
+    return (
+      <>
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+          isCentered
+        >
+          <AlertDialogOverlay />
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              {title}
+            </AlertDialogHeader>
+  
+            <AlertDialogBody>
+              {/* Are you sure? You can't undo this action afterwards. */}
+              {body}
+            </AlertDialogBody>
+  
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                {canceLabel}
+              </Button>
+              <Button colorScheme='red' ml={3} variantColor="red" onClick={action}>
+                {deleteLabel}
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </>
+  );
+}
